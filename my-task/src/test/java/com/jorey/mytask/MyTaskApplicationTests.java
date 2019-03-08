13bdd6b9bb90815590c2a07b15503e4cbbbaef9b
Jorey <18655599240@163.com>
2019-03-09 01:29:53 +0800

    new project
    new project
    
    Co-Authored-By: Jorey <18655599240@163.com>

---
+++ my-task/src/test/java/com/jorey/mytask/MyTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jorey.mytask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyTaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
